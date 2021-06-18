62ff88fe998c804656c2b0daee27c6c2aeb20801
Itamar Ravid <iravid@iravid.com>
2021-06-18 18:30:02 +0300

    Move configuration of serdes to the creation effect

--- src/main/scala/zio/kafka/serde/Serializer.scala
+++ src/main/scala/zio/kafka/serde/Serializer.scala
@@ -1,6 +1,7 @@
 package zio.kafka.serde
 
 import zio.{ RIO, Task }
+import zio.blocking.{ Blocking, blocking => zioBlocking }
 import org.apache.kafka.common.header.Headers
 import org.apache.kafka.common.serialization.{ Serializer => KafkaSerializer }
 
@@ -14,7 +15,6 @@ import scala.jdk.CollectionConverters._
  */
 trait Serializer[-R, -T] {
   def serialize(topic: String, headers: Headers, value: T): RIO[R, Array[Byte]]
-  def configure(props: Map[String, AnyRef], isKey: Boolean): Task[Unit]
 
   /**
    * Create a serializer for a type U based on the serializer for type T and a mapping function
@@ -28,6 +28,15 @@ trait Serializer[-R, -T] {
   def contramapM[R1 <: R, U](f: U => RIO[R1, T]): Serializer[R1, U] =
     Serializer((topic, headers, u) => f(u).flatMap(serialize(topic, headers, _)))
 
+  /**
+   * Returns a new serializer that executes its serialization function on the blocking threadpool.
+   */
+  def blocking: Serializer[R with Blocking, T] =
+    Serializer((topic, headers, t) => zioBlocking(serialize(topic, headers, t)))
+
+  /**
+   * Returns a new serializer that handles optional values and serializes them as nulls.
+   */
   def asOption[U <: T](implicit ev: Null <:< T): Serializer[R, Option[U]] =
     contramap(_.orNull)
 }
@@ -41,18 +50,22 @@ object Serializer extends Serdes {
     new Serializer[R, T] {
       override def serialize(topic: String, headers: Headers, value: T): RIO[R, Array[Byte]] =
         ser(topic, headers, value)
-
-      override def configure(props: Map[String, AnyRef], isKey: Boolean): Task[Unit] = Task.unit
     }
 
   /**
    * Create a Serializer from a Kafka Serializer
    */
-  def apply[T](serializer: KafkaSerializer[T]): Serializer[Any, T] = new Serializer[Any, T] {
-    override def serialize(topic: String, headers: Headers, value: T): Task[Array[Byte]] =
-      Task(serializer.serialize(topic, headers, value))
+  def fromKafkaSerializer[T](
+    serializer: KafkaSerializer[T],
+    props: Map[String, AnyRef],
+    isKey: Boolean
+  ): Task[Serializer[Any, T]] =
+    Task(serializer.configure(props.asJava, isKey))
+      .as(
+        new Serializer[Any, T] {
+          override def serialize(topic: String, headers: Headers, value: T): Task[Array[Byte]] =
+            Task(serializer.serialize(topic, headers, value))
+        }
+      )
 
-    override def configure(props: Map[String, AnyRef], isKey: Boolean): Task[Unit] =
-      Task(serializer.configure(props.asJava, isKey))
-  }
 }
